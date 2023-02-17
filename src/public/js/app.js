fe758ef7c1e96d5dff9582c93c164910636979f1
beckhem96 <beckhem96@naver.com>
2023-02-17 22:14:09 +0900

    socket chat basic

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,17 +1,44 @@
+const messageList = document.querySelector("ul");
+const nickForm = document.querySelector("#nick");
+const messageForm = document.querySelector("#message");
+
 const socket = new WebSocket(`ws://${window.location.host}`);
+socket.binaryType = "blob";
+
+function makeMessage(type, payload) {
+  const msg = { type, payload };
+  return JSON.stringify(msg);
+}
 
 socket.addEventListener("open", () => {
   // socket이 open됐을 때
   console.log("connected to server");
 });
 socket.addEventListener("message", (message) => {
-  console.log("New message:", message.data);
+  const li = document.createElement("li");
+  li.innerText = message.data;
+  messageList.append(li);
 });
 
 socket.addEventListener("close", () => {
   console.log("Disconnected from server");
 });
 
-setTimeout(() => {
-  socket.send("Hello from the browser, ????1111");
-}, 5000);
+function handleSubmit(event) {
+  event.preventDefault();
+  const input = messageForm.querySelector("input");
+  socket.send(makeMessage("new_message", input.value));
+  const li = document.createElement("li");
+  li.innerText = `You : ${input.value}`;
+  messageList.append(li);
+  input.value = "";
+}
+
+function handleNickSubmit(event) {
+  event.preventDefault();
+  const input = nickForm.querySelector("input");
+  socket.send(makeMessage("nickname", input.value));
+}
+
+messageForm.addEventListener("submit", handleSubmit);
+nickForm.addEventListener("submit", handleNickSubmit);
--- src/server.js
+++ src/server.js
@@ -11,19 +11,29 @@ app.get("/", (req, res) => res.render("home"));
 app.get("/*", (req, res) => res.redirect("/"));
 
 const handleListen = () => console.log(`Listning on http://localhost:3000`);
-// app.listen(3000, handleListen);
+
 const server = http.createServer(app); // express.js로 http 서버를 생성
 const wss = new WebSocket.Server({ server }); // WebSocket 객체 안에 http서버 넣어서 http, websocket을 둘다 사용 가능, http서버 위에 ws서버 만들었다고 이해
 // http안 쓰고 싶으면 WebSocket만 쓰면 됨
 // 동일한 포트에서 http, ws req 두개를 처리할 수 있음
-
+const sockets = [];
 wss.on("connection", (socket) => {
+  sockets.push(socket);
+  socket["nickname"] = "Anon";
   console.log("connected to browser");
   socket.on("close", () => console.log("Disconnected from browser"));
-  socket.on("message", (message) => {
-    console.log(message);
+  socket.on("message", (msg) => {
+    const utf8msg = msg.toString("utf8");
+    const message = JSON.parse(utf8msg);
+    switch (message.type) {
+      case "new_message":
+        sockets.forEach((aSocket) =>
+          aSocket.send(`${socket.nickname} : ${message.payload}`)
+        );
+      case "nickname":
+        socket["nickname"] = message.payload;
+    }
   });
-  socket.send("hello!!");
 });
 
 server.listen(3000, handleListen);
